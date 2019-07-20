25f8bfcfc7adb18e8c3413252ae7a7681d6f8197
hscruz <52939231+hscruz@users.noreply.github.com>
2019-07-20 10:29:24 -0300

    mensagem adicionar arquivo woman can code

---
+++ src/test/java/com/womancancode/projeto/final/ProjetoFinalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.womancancode.projeto.final;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjetoFinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
